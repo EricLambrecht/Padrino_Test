0495e3f9b5d51bcf7c00534ca5d5d267b7b059fd
Xannicx <Y.Rietz@gmail.com>
2012-02-16 23:37:39 +0100

    postgresql vorbereitung

--- edelbiter/config/database.rb
+++ edelbiter/config/database.rb
@@ -14,6 +14,6 @@ DataMapper::Property::String.length(255)
 
 case Padrino.env
   when :development then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "edelbiter_development.db"))
-  when :production  then DataMapper.setup(:default, 'postgres://test@test.com:test/edelbiter_production')
+  when :production  then DataMapper.setup(:default, 'postgres://tester:test@localhost/edelbiter_production')
   when :test        then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "edelbiter_test.db"))
 end
