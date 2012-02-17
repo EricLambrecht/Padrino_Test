4aceeee701f30e182bbe5010b43924a68514de09
Keno <kenowessels@googlemail.com>
2012-02-17 17:37:00 +0100

    Bild in Schokotabell eingefuegt

--- edelbiter/app/views/archive/applicationsearch.js
+++
@@ -1,6 +0,0 @@
-  // Hover Effekt   -   Funktioniert
-  $('tbody tr').hover(function(){  
-    $(this).find('td').addClass('hovered');  
-  }, function(){  
-    $(this).find('td').removeClass('hovered');  
-  }); 
