021c6b6c270141c233adde738627b2e941900801
Eric Lambrecht <eric.lambrecht@web.de>
2012-02-19 13:18:11 +0100

    mails senden funktion (nicht fertig)

--- edelbiter/app/controllers/suggest.rb
+++ edelbiter/app/controllers/suggest.rb
@@ -4,7 +4,18 @@ Edelbiter.controllers :suggest do
     render 'suggest/index'
   end
   
-  post :send do
-  
+  post :send do 
+    # Das Gem Mail wird hierfuer benutzt (installiert)
+    Mail.defaults do
+      smtp 'localhost:25' # Port 25 default, testen!
+    end
+     
+    mail = Mail.new do
+          from params[:absender]
+            to 'beschwerden@edelbiter.de'
+       subject params[:betreff]
+          body params[:nachricht]
+    end
+    mail.deliver!
   end
 end
