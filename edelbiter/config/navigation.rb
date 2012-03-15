6ce0551162caefe90e41342d90cfac3d39f7253b
Xannicx <Y.Rietz@gmail.com>
2012-03-15 19:25:34 +0100

    Erneute neugestaltung des Headers

--- edelbiter/config/navigation.rb
+++ edelbiter/config/navigation.rb
@@ -2,7 +2,7 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.item :schokolade, 'SCHOKOLADE', '/archive', :highlights_on => /\/sort/ 
     primary.item :blog, 'BLOG', '/blog', :highlights_on => /\/blog/ 
-    primary.item :about, 'UEBER UNS', '/about'
+    primary.item :about, '&Uuml;BER UNS', '/about'
     primary.item :vorschlag, 'VORSCHLAG', '/suggest'
   end
 end
