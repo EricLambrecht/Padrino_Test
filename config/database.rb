4aea9a9f03c28f39f2e6d6739b5cce34e26e3c84
Björn Jensen <mirouhh@googlemail.com>
2012-05-05 13:46:14 +0200

    Damned. Massive usage of datamapper

--- config/database.rb
+++ config/database.rb
@@ -15,21 +15,21 @@ require 'active_record'
 # config/database.rb
 postgres = URI.parse(ENV['DATABASE_URL'] || '')
 
-ActiveRecord::Base.configurations[:production] = {
-  :adapter  => 'postgresql',
-  :encoding => 'utf8',
-  :database => postgres.path[1..-1], 
-  :username => postgres.user,
-  :password => postgres.password,
-  :host     => postgres.host
-}
+#ActiveRecord::Base.configurations[:production] = {
+#  :adapter  => 'postgresql',
+#  :encoding => 'utf8',
+#  :database => postgres.path[1..-1], 
+#  :username => postgres.user,
+#  :password => postgres.password,
+#  :host     => postgres.host
+#}
 
-#DataMapper.logger = logger
-#DataMapper::Property::String.length(255)
+DataMapper.logger = logger
+DataMapper::Property::String.length(255)
 
 
-#case Padrino.env
-#  when :development then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "edelbiter_development.db"))
-#  when :production  then DataMapper.setup(:default, "postgres://" + postgres.user + ":" + postgres.password + "@" + postgres.host + "/" + postgres.user)
-#  when :test        then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "edelbiter_test.db"))
-#end
+case Padrino.env
+  when :development then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "edelbiter_development.db"))
+  when :production  then DataMapper.setup(:default, "postgres://" + postgres.user + ":" + postgres.password + "@" + postgres.host + "/" + postgres.user)
+  when :test        then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "edelbiter_test.db"))
+end
