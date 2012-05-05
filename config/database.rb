4edc44ce9395ccff804b7db722f4cafff78312f7
Björn Jensen <mirouhh@googlemail.com>
2012-05-05 13:00:23 +0200

    Changing production db configuration

--- config/database.rb
+++ config/database.rb
@@ -19,18 +19,18 @@ DataMapper::Property::String.length(255)
 
 case Padrino.env
   when :development then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "edelbiter_development.db"))
-  when :production  then DataMapper.setup(:default, 'foo:admin@postgres://localhost/mydb')
+  when :production  then DataMapper.setup(:default, "postgres://" + postgres.user + ":" + postgres.password + "@" + postgres.host + "/" + postgres.user)
   when :test        then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "edelbiter_test.db"))
 end
 
 # config/database.rb
-postgres = URI.parse(ENV['DATABASE_URL'] || '')
+#postgres = URI.parse(ENV['DATABASE_URL'] || '')
 
-ActiveRecord::Base.configurations[:production] = {
-  :adapter  => 'postgresql',
-  :encoding => 'utf8',
-  :database => postgres.path[1..-1], 
-  :username => postgres.user,
-  :password => postgres.password,
-  :host     => postgres.host
-}
+#ActiveRecord::Base.configurations[:production] = {
+#  :adapter  => 'postgresql',
+#  :encoding => 'utf8',
+#  :database => postgres.path[1..-1], 
+#  :username => postgres.user,
+#  :password => postgres.password,
+#  :host     => postgres.host
+#}
